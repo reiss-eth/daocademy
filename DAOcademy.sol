c63afe6298eb5b024af63ca6e071908ed04e461d
Daniel Reiss <59092573+reiss-eth@users.noreply.github.com>
2025-02-23 13:52:42 +0700

    Create DAOcademy.sol
    ERC20 Token on Arbitrum

---
+++ DAOcademy.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract DAOcademy is ERC20 {
+    uint256 private constant _maxSupply = 1_000_000_000 * 10 ** 18; // 1 Milliarde BURN
+
+    constructor() ERC20("DAOcademy", "BURN") {
+        _mint(msg.sender, _maxSupply);
+    }
+
+    function maxSupply() public pure returns (uint256) {
+        return _maxSupply;
+    }
+}
